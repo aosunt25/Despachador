0d22828a427af2ee7bb5e753894375467f884e1e
Alfredo Osuna <alfredoosuna40@gmail.com>
2019-10-13 22:14:25 -0500

    a

--- Despachador.py
+++ Despachador.py
@@ -1,36 +1,37 @@
 import math
+from Procesos import Procesos
+import tkinter as tk
+from tkinter import ttk
 
 class Microprocesador():
-    procesos = []
+    
+    
     #Class Attribute 
     def __init__(self, num):
         self.num = num
+        self.procesos = []
+        self.cantidadProceso = len(self.procesos)
 
     def anadirProceso(self, proceso):
         self.procesos.append(proceso)
 
 
-class Proceso():
-
-    #Class Attribute 
-    def __init__(self, name,tcc, tvc, tiempoEj, tb, ti, tt, tf):
-        self.name = name
-        self.tcc = tcc
-        self.tvc = tvc
-        self.tb = tb
-        self.ti = ti
-        self.tt = tt
-        self.tf = tf
-        self.tiempoEjecucion = tiempoEj
 
 
 class Despachador():
     pass
     microprocesadores = []
+    listaDeProcesos = []
+    numDeMicro = None
+    entryMicro = None
+    entryQuantums = None
+
+    
 
     def aniadirMicroprocesador(self, num):
         micro = Microprocesador(num)
         self.microprocesadores.append(micro)
+
     
     def aniadirProcesos(self, duracionQuantum, num, names, tiemposEjecucion, numBloqueos, duracionBloqueo, duracionCambio):
 
@@ -40,9 +41,10 @@ class Despachador():
             tvc = ((math.ceil(tiemposEjecucion[i] / float(duracionQuantum))) - 1) * duracionCambio
 
             tb = numBloqueos[i]*duracionBloqueo
+
             if i == 0:
                 tt = tiemposEjecucion[i] + tvc + numBloqueos[i]*duracionBloqueo  
-                proceso = Proceso(names[i], 0, tvc, tiemposEjecucion[i], tb, 0, tt, tt)
+                proceso = Procesos(names[i], 0, tvc, tiemposEjecucion[i], tb, 0, tt, tt)
 
                 self.microprocesadores[0].procesos.append(proceso)
 
@@ -50,7 +52,7 @@ class Despachador():
                 #AQUI SE DEBE CHECAR QUE MICRO TIENE MENOS TIEMPO FINAL EN SU ULTIMA POSICION DE PROCESOS .TF
                 tt = duracionCambio + tiemposEjecucion[i] + tvc + numBloqueos[i]*duracionBloqueo
                 ti = self.microprocesadores[0].procesos[i-1].tf
-                proceso = Proceso(names[i], duracionCambio, tvc, tiemposEjecucion[i], tb, ti, tt, ti + tt)
+                proceso = Procesos(names[i], duracionCambio, tvc, tiemposEjecucion[i], tb, ti, tt, ti + tt)
                 self.microprocesadores[0].procesos.append(proceso)
 
         #Aqui checar si hay mas de un micro
@@ -63,9 +65,64 @@ class Despachador():
             for j in range(len(self.microprocesadores[i].procesos)):
                 print("      " +self.microprocesadores[i].procesos[j].name + "      " +  str(self.microprocesadores[i].procesos[j].tcc) + "      " +str(self.microprocesadores[i].procesos[j].tiempoEjecucion) +"     " + str(self.microprocesadores[i].procesos[j].tvc) + "      " +str(self.microprocesadores[i].procesos[j].tb) +"     " + str(self.microprocesadores[i].procesos[j].tt) +"    " + str(self.microprocesadores[i].procesos[j].ti) +"   " + str(self.microprocesadores[i].procesos[j].tf))
 
-def main():
-    despachador = Despachador()
 
+    def lecturaDeArchivo(self):
+        archivo = open("proceso.txt", "r")
+        if archivo.mode == 'r':
+           linea=archivo.readlines()
+           for x in linea:
+                lineaActual = x.split(',')
+                print(lineaActual[0], " ", lineaActual[1], " ", lineaActual[2])
+                proceso = Procesos(lineaActual[0],lineaActual[1],lineaActual[2],0,0,0,0,0)
+                self.listaDeProcesos.append(proceso)
+            
+        for i in range(len(self.listaDeProcesos)):
+            print(self.listaDeProcesos[i])
+
+    def prueba(self):
+        self.numDeMicro = self.entryMicro.get()
+        print(self.numDeMicro)
+        print("hola l")
+
+    def interfaz (self):
+        root = tk.Tk()
+        root.config(width=300, height=200)
+        
+        #grid = Grid()
+        labelQuantums = tk.Label(root, text = "Quantums").grid(row = 0)
+        self.entryQuantums = tk.Entry(root).grid(row = 0, column = 1)
+        labelMicro = tk.Label(root, text = "Numero de micros").grid(row = 1)
+        self.entryMicro = tk.Entry(root)
+        self.entryMicro.grid(row = 1 , column = 1)
+        frame_canvas = tk.Frame(root)
+        frame_canvas.grid(row=7 , column=0, pady=(5, 0), sticky='nw')
+        frame_canvas.grid_rowconfigure(0, weight=1)
+        frame_canvas.grid_columnconfigure(0, weight=1)
+        canvas = tk.Canvas(frame_canvas, bg="gray")
+        canvas.grid(row=0, column=0, sticky="news")
+        rows = len(self.listaDeProcesos)
+        columns = 3
+        #procesos = [[tk.Lable() for j in range(columns)] for i in range(rows)]
+        '''
+        for i in range(0, rows):
+            procesoNom = tk.Label (text = self.listaDeProcesos[i].name)
+            procesoNom.grid(row = i, columns=0)
+            proceso = tk.Label (text = self.listaDeProcesos[i].tt)
+            procesoNom.grid(row = i, columns=1)
+        '''
+
+            
+        
+        button = ttk.Button(root,text ="Mostrar Tabla", command = self.prueba)
+        button.grid(row = 6)
+        button.pack
+        root.mainloop()
+
+
+def main():
+    
+    despachador = Despachador()
+    despachador.lecturaDeArchivo()
     #Todo esto se va leer del txt
     numeroProcesos = 17
     numeroMicros = 1
@@ -82,7 +139,8 @@ def main():
         despachador.aniadirMicroprocesador(i+1)
 
     despachador.aniadirProcesos(duracionQuantum, numeroProcesos, nombres, tiemposEjecucion, numBloqueos, tiempoBloqueo, tiempoCambio)
-    despachador.imprimirEstado()
+    despachador.interfaz()
+    #despachador.imprimirEstado()
 
 
 
---
+++ Procesos.py
@@ -0,0 +1,26 @@
+class Procesos:
+
+    '''
+    def __init__(self, nombre, tiempoTotal, tiempoDeEntrada, numeroDeProceso, tiempoDeBloque, tiempoFinal, tcc, tvc, ti):
+        self.nombre = nombre
+        self.tiempoTotal = tiempoTotal
+        self.tiempoDeEntrada = tiempoDeEntrada
+        self.tiempoDeBloque = tiempoDeBloque
+        self.tcc = tcc
+        self.tvc = tvc
+        self.ti = ti
+        self.tiempoFinal = tiempoFinal
+        self.numeroDeProceso = numeroDeProces
+       
+
+    #Class Attribute 
+    '''
+    def __init__(self, name, tt, tiempoEj, tcc, tvc, tb, ti, tf):
+        self.name = name
+        self.tcc = tcc
+        self.tvc = tvc
+        self.tb = tb
+        self.ti = ti
+        self.tt = tt
+        self.tf = tf
+        self.tiempoEjecucion = tiempoEj
